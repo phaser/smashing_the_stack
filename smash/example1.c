76454daa46e680638d39bc184dab6aab7612a9c5
Cristian Bidea <cristian.bidea@keysight.com>
2019-10-11 15:39:53 +0300

    compiler flags

--- smash/example1.c
+++ smash/example1.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+
+void function (int a, int b, int c) {
+    char buffer1[5];
+    int *ret;
+
+    ret = buffer1 + 17;
+    (*ret) += 7;
+}
 
 int main(int argc, char** argv) {
+    int x;
+
+    x = 0;
+    function(1, 2, 3);
+    x = 1;
+    printf("%d\n", x);
     return 0;
 }
