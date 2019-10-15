c570a229b5b42314865fab4335db706cab8a65b1
Cristian Bidea <cristian.bidea@keysight.com>
2019-10-15 17:00:09 +0300

    started exploit, not working

--- smash/vulnerable.c
+++ smash/vulnerable.c
@@ -1,8 +1,14 @@
 /* A simple vulnerable program, that does an unbounded strcpy */
 #include <string.h>
+#include <sys/mman.h>
+#include <stdint.h>
+#include <stdio.h>
+
+char buffer[512];
 
 void main(int argc, char *argv[]) {
-    char buffer[512];
-    if (argc > 1) 
-        strcpy(buffer,argv[1]);
+    mprotect((void *)((uint64_t)buffer & ~4095), 4096, PROT_READ | PROT_WRITE | PROT_EXEC);
+    if (argc > 1) {
+        strcpy(buffer, argv[1]);
+    }
 }
